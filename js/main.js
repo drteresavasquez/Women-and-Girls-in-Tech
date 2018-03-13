9e71e37bc11d22330d840dfa4f5bd683669a9c95
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-03-13 15:45:51 -0500

    updates

--- js/main.js
+++ js/main.js
@@ -12,3 +12,12 @@ require("./navElements");
 
 
 //https://medium.freecodecamp.org/the-6-most-desirable-coding-jobs-and-the-types-of-people-drawn-to-each-aebac45fd7f7
+
+//https://learntocodewith.me/posts/become-a-software-engineer/
+
+//https://code.org/girls
+//https://code.org/careers-in-tech
+//https://code.org/educate/resources/inspire#posters
+//https://www.ncwit.org/infographic/3435
+
+//https://www.ncwit.org/sites/default/files/resources/c4c_pathwaysposter_24x36_print.pdf
