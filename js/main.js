d206b6fade8a984a26684a0e9728781e7df7efff
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-03-14 14:13:40 -0500

    added meta and money forms

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
 "use strict";
 
 require("./navElements");
-
-// Reource List Links
-// https://www.16personalities.com/free-personality-test
-// https://freecodecamp.org
-// https://www.codewars.com/
-// http://flexboxfroggy.com/
-// https://codecombat.com/
-// https://codeacademy.com
-
-
-//https://medium.freecodecamp.org/the-6-most-desirable-coding-jobs-and-the-types-of-people-drawn-to-each-aebac45fd7f7
-
-//https://learntocodewith.me/posts/become-a-software-engineer/
-
-//https://code.org/girls
-//https://code.org/careers-in-tech
-//https://code.org/educate/resources/inspire#posters
-//https://www.ncwit.org/infographic/3435
-
-//https://www.ncwit.org/sites/default/files/resources/c4c_pathwaysposter_24x36_print.pdf
